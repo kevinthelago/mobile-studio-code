2b7b30a5735e469bf8ac7c4371b41bb945049d85
Kevin Lago <kevinthelago@gmail.com>
2026-05-09 18:32:01 -0400

    msc: edits from Mobile Studio Code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import { BottomTabBar } from '../../src/components/ui/BottomTabBar';
 
 export default function TabsLayout() {
-  // Native screen background is painted by ThemedFrame in app/_layout.tsx;
-  // BottomTabNavigationOptions doesn't accept screenBackgroundColor /
-  // contentStyle in the installed expo-router version, so we don't try.
   return (
     <Tabs
       screenOptions={{ headerShown: false }}
       tabBar={(props) => <BottomTabBar {...props} />}
+      sceneContainerStyle={{ backgroundColor: 'transparent' }}
     >
       <Tabs.Screen name="index" options={{ title: 'Files' }} />
       <Tabs.Screen name="find" options={{ title: 'Find' }} />
